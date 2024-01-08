31b4f821836836b3cb76c7cafac9234f10fe50dd
sahilanand72 <118097650+sahilanand72@users.noreply.github.com>
2024-01-08 18:57:33 +0530

    deploy try 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Food-Delivery-Website/",
   plugins: [react()],
-  base: "/Food-Delivery-Website/"
+  
 })
